b6c984dd3baad3ab0a01b54a0c72e4a1c171caa7
Andrew Edward <aedward@andrew.cmu.edu>
2019-04-24 15:14:26 +0000

    Filled factories file with basic defaults from the requirements sheet

--- test/factories.rb
+++ test/factories.rb
@@ -1,31 +1,63 @@
 FactoryBot.define do
  
-  factory :employee do
-    first_name { "Ed" }
-    last_name { "Gruberman" }
-    ssn { rand(9 ** 9).to_s.rjust(9,'0') }
-    date_of_birth { 19.years.ago.to_date }
-    phone { rand(10 ** 10).to_s.rjust(10,'0') }
-    role { "employee" }
-    active { true }
-  end
+    factory :employee do
+        first_name { "Alex" }
+        last_name { "Heimann" }
+        ssn { rand(9 ** 9).to_s.rjust(9,'0') }
+        date_of_birth { 19.years.ago.to_date }
+        phone { rand(10 ** 10).to_s.rjust(10,'0') }
+        role { "admin" }
+        active { true }
+    end
+      
+    factory :store do
+        name {"CMU"}
+        street {"5000 Forbes Avenue"}
+        city {"Pittsburgh"}
+        state {"PA"}
+        zip {"15213"}
+        phone { rand(10 ** 10).to_s.rjust(10,'0') }
+        active {true}
+    end
+      
+    factory :assignment do
+        association :store
+        association :employee
+        start_date {1.year.ago.to_date}
+        end_date {1.month.ago.to_date}
+        pay_level {1}
+    end
   
-  factory :store do
-    name {"CMU"}
-    street {"5000 Forbes Avenue"}
-    city {"Pittsburgh"}
-    state {"PA"}
-    zip {"15213"}
-    phone { rand(10 ** 10).to_s.rjust(10,'0') }
-    active {true}
-  end
+    factory :shift do
+        association :assignment
+        date {Date.today}
+        start_time {"11:00am".to_time}
+        end_time {"2:00pm".to_time}
+        notes {"Johnny was a boy on this shift"}
+    end
+    
+    factory :shift_job do
+        association :shift
+        association :job
+    end
   
-  factory :assignment do
-    association :store
-    association :employee
-    start_date {1.year.ago.to_date}
-    end_date {1.month.ago.to_date}
-    pay_level {1}
-  end
-
+    factory :job do
+        name {"Work cash register"}
+        description {"Serving ice cream is a hard task"}
+        active {true}
+    end
+    
+    factory :flavor do
+        name {"Chocolate"}
+        active {true}
+    end
+    
+    factory :store_flavor do
+        association :store
+        association :flavor
+    end
+    
+    factory :user do
+    
+    end
 end
